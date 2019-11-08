2ad612a4a8dd139d13acbc4963553556102435c7
StepanHokobyan <ha.stepan02@gmail.com>
2019-11-08 14:12:15 +0400

    mvvm first commit

---
+++ MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UwpMVVM
+{
+    public class MainViewModel : MainViewModelBase
+    {
+        private Calculator calculator_ = null;
+        private int result_ = 0;
+        private int value1_ = 0;
+        private int value2_ = 0;
+        private bool isAddChecked = false;
+        private bool isSubChecked = false;
+        private bool isMulChecked = false;
+        private bool isDivChecked = false;
+    }
+}
